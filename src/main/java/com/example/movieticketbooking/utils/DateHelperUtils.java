2fcebf4fc79fbc9aafecabe46235086c0f72c8fc
NgoKiet97 <ntkiet97dev@gmail.com>
2023-08-27 17:25:45 +0700

    update api/add-show-moive

--- src/main/java/com/example/movieticketbooking/utils/DateHelperUtils.java
+++ src/main/java/com/example/movieticketbooking/utils/DateHelperUtils.java
@@ -1,6 +1,8 @@
 package com.example.movieticketbooking.utils;
 
+import java.text.DateFormat;
 import java.text.Format;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -8,5 +10,10 @@ public class DateHelperUtils {
     public String formatDateToString(Date date){
         Format formatter = new SimpleDateFormat("dd-MM-yyyy");
         return formatter.format(date);
+    }
+
+    public Date formatStringToDate(String date) throws ParseException {
+        DateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        return formatter.parse(date);
     }
 }
