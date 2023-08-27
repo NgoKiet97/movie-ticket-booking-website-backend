b2742c2bb00111a484bd974cd00d26a8e2c66cc3
NgoKiet97 <ntkiet97dev@gmail.com>
2023-08-27 16:15:06 +0700

    update API/get-all-ticket

--- src/main/java/com/example/movieticketbooking/payload/response/TicketResponse.java
+++ src/main/java/com/example/movieticketbooking/payload/response/TicketResponse.java
@@ -6,10 +6,19 @@ import lombok.Setter;
 @Getter @Setter
 public class TicketResponse {
     private int showMovieId;
+    private String movie;
+    private String cinema;
+    private String room;
+    private String date;
+    private String startTime;
+
     private String seat;
     private String guestName;
     private String typeTicket;
     private int billId;
+    private String user;
+
     private String stateTicket;
+
 
 }
--- src/main/java/com/example/movieticketbooking/service/TicketService.java
+++ src/main/java/com/example/movieticketbooking/service/TicketService.java
@@ -6,6 +6,7 @@ import com.example.movieticketbooking.payload.response.RoleResponse;
 import com.example.movieticketbooking.payload.response.TicketResponse;
 import com.example.movieticketbooking.repository.TicketRepository;
 import com.example.movieticketbooking.service.imp.ITicketService;
+import com.example.movieticketbooking.utils.DateHelperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,10 +26,20 @@ public class TicketService implements ITicketService {
         for(TicketEntity entity : ticketEntityList){
             TicketResponse ticketResponse = new TicketResponse();
             ticketResponse.setShowMovieId(entity.getShowMovie().getId());
+            ticketResponse.setMovie(entity.getShowMovie().getMovie().getName());
+            ticketResponse.setCinema(entity.getShowMovie().getRoomEntity().getCinema().getName());
+            ticketResponse.setRoom(entity.getShowMovie().getRoomEntity().getName());
+
+            DateHelperUtils dateHelperUtils = new DateHelperUtils();
+            ticketResponse.setDate(dateHelperUtils.formatDateToString(entity.getShowMovie().getDate()));
+
+            ticketResponse.setStartTime(entity.getShowMovie().getStartTime().toString());
+
             ticketResponse.setSeat(entity.getSeat().getName());
             ticketResponse.setGuestName(entity.getGuestName());
             ticketResponse.setTypeTicket(entity.getTypeTicket().getName());
             ticketResponse.setBillId(entity.getBill().getId());
+            ticketResponse.setUser(entity.getBill().getUser().getName());
             ticketResponse.setStateTicket(entity.getStateTicket().getName());
 
             ticketResponseList.add(ticketResponse);
